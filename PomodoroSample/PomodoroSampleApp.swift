c6f54bcfa3c5f3006ba8e8313900f44f97484cfe
Kame2021 <21cm0108@jec.ac.jp>
2022-12-09 12:19:22 +0900

    Initial Commit

---
+++ PomodoroSample/PomodoroSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomodoroSampleApp.swift
+//  PomodoroSample
+//
+//  Created by Ryo on 2022/12/09.
+//
+
+import SwiftUI
+
+@main
+struct PomodoroSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
